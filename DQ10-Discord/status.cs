76570e4df68e35bbebcf90ad999dcc33e15531e9
yuu111 <yuu210yuu@gmail.com>
2018-08-08 18:30:26 +0900

    スライムの追加

--- DQ10-Discord/status.cs
+++ DQ10-Discord/status.cs
@@ -10,14 +10,19 @@ namespace DQ10_Discord
     {
 
         public static String mes2;
+        public static String grave = "```";
         public static void MonsterStatus()
         {
 
             if (Program.mes == "スライム") {
 
-                mes2 = "スライム\nテスト";
-            }
+                mes2 = grave+ "スライム\n経験値:2P\nゴールド:1G\n";
+                mes2 += "\nHP:5\nMP:2\nATK:9\nDEF:11\n重さ80\n";
+                mes2 += "\n装備\nどうのつるぎ\n白木のおうぎ\n皮の盾\n前座芸人の服\n前座芸人のながぐつ\n";
+                mes2 += "\n宝珠\nなし" + grave;
 
+
+            }
 
         }
     
