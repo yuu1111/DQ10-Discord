7b5a6f5b43fafcfdd03179403f1ac2b46e0f99cb
yuu111 <yuu210yuu@gmail.com>
2018-08-09 15:21:30 +0900

    コードの一部変更

--- DQ10-Discord/status.cs
+++ DQ10-Discord/status.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static DQ10_Discord.Program;
+using static DQ10_Discord.Weapon;
+using static DQ10_Discord.Area;
 
 namespace DQ10_Discord
 {
@@ -17,10 +20,11 @@ namespace DQ10_Discord
         public static int Weght;
         public static String mes2;
         public static String grave = "```";
+
         public static void MonsterStatus()
 
         {
-            if (Program.mes == "スライム") {
+            if (mes == "スライム") {
                 EXP = 2;
                 GOLD = 1;
                 HP = 5;
@@ -28,17 +32,29 @@ namespace DQ10_Discord
                 ATK = 9;
                 DEF = 11;
                 Weght = 80;
-                mes2 = grave+ "スライム\n経験値:"+EXP+"P\nゴールド:"+GOLD+"G\n";
-                mes2 += "\nHP:"+HP+"\nMP:"+MP+"\nATK:"+ATK+"\nDEF:"+DEF+"\n重さ:"+Weght+"\n";
-                mes2 += "\n装備\n"+ Weapon.Sword[1] + "\n白木のおうぎ\n皮の盾\n前座芸人の服\n前座芸人のながぐつ\n";
-                mes2 += "\n宝珠\nなし\n";
-                mes2 += "\n出現場所";
-                mes2 += "\n" + Area.Orgreede[1];
-                mes2 += "\n" + Area.Eltona[1];
-                mes2 += "\n" + Area.Dwachakka[1];
-                mes2 += "\n" + Area.Dwachakka[2];
-                mes2 += "\n" + Area.Pukland[1];
-                mes2 += "\n" + Area.Wenna[1];
+                mes2 = grave + "スライム";
+                mes2 += "\n" + "経験値:"+EXP;
+                mes2 += "\n" + "ゴールド:" + GOLD;
+                mes2 += "\n"+ "HP:" + HP;
+                mes2 += "\n" +"MP:" + MP;
+                mes2 += "\n" +"ATK:" + ATK;
+                mes2 += "\n" +"DEF:" + DEF;
+                mes2 += "\n" +"重さ:" + Weght;
+                mes2 += "\n\n" + "装備";
+                mes2 += "\n" + Sword[1];
+                mes2 += "\n" +"白木のおうぎ";
+                mes2 += "\n" + "皮の盾";
+                mes2 += "\n" + "前座芸人の服";
+                mes2 += "\n" +"前座芸人のながぐつ";
+                mes2 += "\n\n" +"宝珠";
+                mes2 += "\n" +"なし";
+                mes2 += "\n\n" +"出現場所";
+                mes2 += "\n" + Orgreede[1];
+                mes2 += "\n" + Eltona[1];
+                mes2 += "\n" + Dwachakka[1];
+                mes2 += "\n" + Dwachakka[2];
+                mes2 += "\n" + Pukland[1];
+                mes2 += "\n" + Wenna[1];
                 mes2 += "\n" + "育みの大地[偽]";
                 mes2 += "\n" + "育みの大地[真]";
                 mes2 += "\n" + "サザミレ草原";
@@ -46,7 +62,7 @@ namespace DQ10_Discord
                 mes2 += "\n" + "辺境の雪山" + grave;
 
             }
-            if (Program.mes == "スライムベス")
+            if (mes == "スライムベス")
             {
 
                 mes2 =  "Test";
